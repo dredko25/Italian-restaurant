98df877841d1279408a79490c1d4087b3a40bf13
dredko25 <dredko25@gmail.com>
2022-12-06 13:48:17 +0100

    add server connection

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+    $host = 'localhost';
+    $db = 'Italian_restaurant';
+    $user = 'root';
+    $pass = ')8v0OlKjf/SvN]3*';
+    $charset = 'utf8';
+
+    $connection = mysqli_connect($host, $user, $pass, $db);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+?>
