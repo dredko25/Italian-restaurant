54825baec184dd36502caaf6246cc65977936627
dredko25 <dredko25@gmail.com>
2022-12-06 14:36:54 +0100

    fix format

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const menuList = document.querySelector(".menu-list");
 const headerMenu = document.querySelector(".header-menu");
 
 burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    menuList.classList.toggle("active");
-    headerMenu.classList.toggle("active");
+  burger.classList.toggle("active");
+  menuList.classList.toggle("active");
+  headerMenu.classList.toggle("active");
 })
 document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click", () => {
-    burger.classList.remove("active");
-    menuList.classList.remove("active");
-    headerMenu.classList.remove("active");
+  burger.classList.remove("active");
+  menuList.classList.remove("active");
+  headerMenu.classList.remove("active");
 }))
 
 let slideIndex = 1;
@@ -29,12 +29,12 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("slide");
   if (n > slides.length) {
     slideIndex = 1
-    }
+  }
   if (n < 1) {
     slideIndex = slides.length
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
